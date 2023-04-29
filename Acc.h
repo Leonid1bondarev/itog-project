6e21713319ebd8dda287f9ac33aab30ac73348cc
qwerty2024 <130404273+qwerty2024@users.noreply.github.com>
2023-04-29 12:39:16 +0300

    Update Acc.h
    Добавил класс для хранения всех аккаунтов
    Добавил геттеры для класса Account

--- Acc.h
+++ Acc.h
@@ -1,14 +1,51 @@
 #pragma once
+
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
-class account
+
+class Account
 {
 public:
-	account(string name, string login, string password);
-	account(string login, string password);
+	Account(string name, string login, string password);
+	Account(string login, string password);
+
+	string get_name() { return _name; }
+	string get_login() { return _login; }
+	string get_password() { return _password; }
+
 protected:
 	string _name;
 	string _login;
 	string _password;
 };
+
+class Accounts_data
+{
+public:
+	Accounts_data()
+	{
+		count = 0;
+	}
+
+	inline int count_acc() { return count; }
+	void add_acc(Account new_acc)
+	{
+		count++;
+		data.push_back(new_acc);
+	}
+
+	//void rem_acc(); // пока не нужно, но на будущее
+
+	bool contains(string login); // проверка, зареган такой аккаунт или нет
+
+	Account get_acc(); //TO DO
+
+protected:
+	int count; // количество имиеющихся аккаунтов
+	vector<Account> data;
+};
+
+
