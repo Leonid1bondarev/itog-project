e46c28dd74153c08f6d3b99ade90fc6f707fba6e
qwerty2024 <130404273+qwerty2024@users.noreply.github.com>
2023-04-30 23:20:55 +0300

    Update Acc.cpp
    Вернул верное содержимое с некоторыми методами.

--- Acc.cpp
+++ Acc.cpp
@@ -1,103 +1,97 @@
-#pragma once
+#include "Acc.h"
 
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-class Message
+bool Accounts_data::containsLog(string login) //ìîæíî ñþäà äîáàâèòü âëîæåííûé öèêë, ÷òîáû â êàæäîé èòòåðàöèè öèêëà ïðîâåðÿòü åù¸ è èìÿ
 {
-public:
-	Message(string mess, int id_send, int id_recv) : _mess(mess), _id_send(id_send), _id_recv(id_recv) {}
+	bool rezult = false;
 
-	string get_mess() { return _mess; }
-	int get_id_send() { return _id_send; }
-
-private:
-	string _mess;
-	int _id_send;
-	int _id_recv;
-};
-
-class Account
-{
-public:
-	Account() : _name(""), _login(""), _password(""), my_id(-1) {}
-	//Account(string name, string login, string password);
-	//Account(string login, string password);
-
-	string get_name() { return _name; }
-	string get_login() { return _login; }
-	string get_password() { return _password; }
-
-	void set_name(string name) { _name = name; }
-	void set_login(string login) { _login = login; }
-	void set_password(string password) { _password = password; }
-	void set_my_id(int id) { my_id = id; }
-
-	//string srm(); //func show received messages
-	//string ssm();
-
-	//void add_rm(string message)
-	//{
-	//	crm++;
-	//	received_message.push_back(message);
-	//}
-	//void add_sm(string message)
-	//{
-	//	csm++;
-	//	sent_message.push_back(message);
-	//}
-
-	void set_recv_mess(string mess, int id_sender);
-	void set_send_mess(string mess, int id_recver);
-	//string get_recv_mess();
-
-	//void print_all_send_massage();
-	void print_recv_massage();
-
-
-protected:
-	int my_id;
-	string _name;
-	string _login;
-	string _password;
-
-	vector<Message> recv_mess;
-	vector<Message> send_mess;
-
-	//vector<string> received_message;
-	//int crm = 0; //counter received messages
-	//vector<string> sent_message;
-	//int csm = 0; //counter sent messages
-};
-
-class Accounts_data
-{
-public:
-	Accounts_data()
+	for (int i = 0; i < count; ++i)
 	{
-		count = 0;
+		if (data[i].get_login() == login)
+		{
+			rezult = true;
+			break;
+		}
+	}
+	return rezult;
+}
+
+bool Accounts_data::containsName(string name)
+{
+	bool rezult = false;
+
+	for (int i = 0; i < count; ++i)
+	{
+		if (data[i].get_name() == name)
+		{
+			rezult = true;
+			break;
+		}
+	}
+	return rezult;
+}
+
+int Accounts_data::get_id_by_login(string login)
+{
+	for (int i = 0; i < count; ++i)
+	{
+		if (data[i].get_login() == login)
+		{
+			return i;
+		}
 	}
 
-	inline int count_acc() { return count; }
+	return (-1);
+}
 
-	void add_acc(Account new_acc)
+int Accounts_data::get_id_by_name(string name)
+{
+	for (int i = 0; i < count; ++i)
 	{
-		count++;
-		data.push_back(new_acc);
+		if (data[i].get_name() == name)
+		{
+			return i;
+		}
 	}
 
-	bool containsLog(string login); // ïðîâåðêà, çàðåãàí òàêîé àêêàóíò èëè íåò
-	int get_id_by_login(string login); // id ýòîãî ëîãèíà (íîìåð)
-	int get_id_by_name(string name);
+	return (-1);
+}
 
-	bool containsName(string name);
+void Account::set_recv_mess(string mess, int id_sender)
+{
+	Message tmp(mess, id_sender, my_id);
+	recv_mess.push_back(tmp);
+}
 
-	Account& operator[](int i) { return data[i]; }
+void Account::set_send_mess(string mess, int id_recver)
+{
+	Message tmp(mess, my_id, id_recver);
+	send_mess.push_back(tmp);
+}
 
-protected:
-	int count; // êîëè÷åñòâî èìèåþùèõñÿ àêêàóíòîâ
-	vector<Account> data;
-};
+void Account::print_recv_massage()
+{
+	for (int i = 0; i < recv_mess.size(); ++i)
+	{
+		cout << "id sender = " << recv_mess[i].get_id_send() << "  msg: " << recv_mess[i].get_mess() << endl;
+	}
+}
+
+//string Account::srm()
+//{
+//	for (int i = 0; i < crm; i++)
+//	{
+//		cout << received_message[i] << endl;
+//	}
+//
+//	return "AAAA"; //затычка
+//}
+//
+//string Account::ssm()
+//{
+//	for (int i = 0; i < csm; i++)
+//	{
+//		cout << sent_message[i] << endl;
+//	}
+//
+//	return "AAAA"; //затычка
+//}
