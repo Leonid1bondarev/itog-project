94cc6cb696f126326748ef32c8bdc270e0fc18de
qwerty2024 <130404273+qwerty2024@users.noreply.github.com>
2023-04-30 23:20:17 +0300

    Update Acc.cpp
    Добавил класс сообщений. Добавил несколько методов класса аккаунт.

--- Acc.cpp
+++ Acc.cpp
@@ -1,60 +1,103 @@
-#include "Acc.h"
+#pragma once
 
-bool Accounts_data::containsLog(string login) //можно сюда добавить вложенный цикл, чтобы в каждой иттерации цикла проверять ещё и имя
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Message
 {
-	bool rezult = false;
+public:
+	Message(string mess, int id_send, int id_recv) : _mess(mess), _id_send(id_send), _id_recv(id_recv) {}
 
-	for (int i = 0; i < count; ++i)
+	string get_mess() { return _mess; }
+	int get_id_send() { return _id_send; }
+
+private:
+	string _mess;
+	int _id_send;
+	int _id_recv;
+};
+
+class Account
+{
+public:
+	Account() : _name(""), _login(""), _password(""), my_id(-1) {}
+	//Account(string name, string login, string password);
+	//Account(string login, string password);
+
+	string get_name() { return _name; }
+	string get_login() { return _login; }
+	string get_password() { return _password; }
+
+	void set_name(string name) { _name = name; }
+	void set_login(string login) { _login = login; }
+	void set_password(string password) { _password = password; }
+	void set_my_id(int id) { my_id = id; }
+
+	//string srm(); //func show received messages
+	//string ssm();
+
+	//void add_rm(string message)
+	//{
+	//	crm++;
+	//	received_message.push_back(message);
+	//}
+	//void add_sm(string message)
+	//{
+	//	csm++;
+	//	sent_message.push_back(message);
+	//}
+
+	void set_recv_mess(string mess, int id_sender);
+	void set_send_mess(string mess, int id_recver);
+	//string get_recv_mess();
+
+	//void print_all_send_massage();
+	void print_recv_massage();
+
+
+protected:
+	int my_id;
+	string _name;
+	string _login;
+	string _password;
+
+	vector<Message> recv_mess;
+	vector<Message> send_mess;
+
+	//vector<string> received_message;
+	//int crm = 0; //counter received messages
+	//vector<string> sent_message;
+	//int csm = 0; //counter sent messages
+};
+
+class Accounts_data
+{
+public:
+	Accounts_data()
 	{
-		if (data[i].get_login() == login)
-		{
-			rezult = true;
-			break;
-		}
+		count = 0;
 	}
-	return rezult;
-}
 
-bool Accounts_data::containsName(string name)
-{
-	bool rezult = false;
+	inline int count_acc() { return count; }
 
-	for (int i = 0; i < count; ++i)
+	void add_acc(Account new_acc)
 	{
-		if (data[i].get_name() == name)
-		{
-			rezult = true;
-			break;
-		}
-	}
-	return rezult;
-}
-
-int Accounts_data::get_id_by_login(string login)
-{
-	for (int i = 0; i < count; ++i)
-	{
-		if (data[i].get_login() == login)
-		{
-			return i;
-		}
+		count++;
+		data.push_back(new_acc);
 	}
 
-	return (-1);
-}
+	bool containsLog(string login); // ГЇГ°Г®ГўГҐГ°ГЄГ , Г§Г Г°ГҐГЈГ Г­ ГІГ ГЄГ®Г© Г ГЄГЄГ ГіГ­ГІ ГЁГ«ГЁ Г­ГҐГІ
+	int get_id_by_login(string login); // id ГЅГІГ®ГЈГ® Г«Г®ГЈГЁГ­Г  (Г­Г®Г¬ГҐГ°)
+	int get_id_by_name(string name);
 
-string Account::srm()
-{
-		for (int i = 0; i < crm; i++)
-		{
-			cout << received_message[i] << endl;
-		}
-}
+	bool containsName(string name);
 
-string Account::ssm()
-{
-	for (int i = 0; i < csm; i++)
-	{
-		cout << sent_message[i] << endl;
-	}
-}
+	Account& operator[](int i) { return data[i]; }
+
+protected:
+	int count; // ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГ® ГЁГ¬ГЁГҐГѕГ№ГЁГµГ±Гї Г ГЄГЄГ ГіГ­ГІГ®Гў
+	vector<Account> data;
+};
