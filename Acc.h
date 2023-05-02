a7e72cff7f9ed96076a65ce8c06c25ac4e4f366d
qwerty2024 <130404273+qwerty2024@users.noreply.github.com>
2023-05-02 22:19:15 +0300

    Update Acc.h
    void print_all_send_massage(); изменил название
    выкинул лишний коментнутый код
    добавил два метода для проверки на пустоту вектора сообщений

--- Acc.h
+++ Acc.h
@@ -9,22 +9,16 @@ using namespace std;
 class Message
 {
 public:
-	//Message(string mess, int id_send, int id_recv) : _mess(mess), _id_send(id_send), _id_recv(id_recv) {}
 	Message(string mess, string name_send, string name_recv) : _mess(mess), _name_send(name_send), _name_recv(name_recv) {}
 
 	string get_mess() { return _mess; }
-	//int get_id_send() { return _id_send; }
-	//int get_id_recv() { return _id_recv; }
 	string get_name_send() { return _name_send; }
 	string get_name_recv() { return _name_recv; }
 
 private:
 	string _mess;
-	//int _id_send;
-	//int _id_recv;
 	string _name_send;
 	string _name_recv;
-
 };
 
 class Account
@@ -36,20 +30,29 @@ public:
 	string get_login() { return _login; }
 	string get_password() { return _password; }
 
-	void set_name(string name) { _name = name; }
-	void set_login(string login) { _login = login; }
-	void set_password(string password) { _password = password; }
-	void set_my_id(int id) { my_id = id; }
-
-	//void set_recv_mess(string mess, int id_sender);
-	//void set_send_mess(string mess, int id_recver);
+	inline void set_name(string name) { _name = name; }
+	inline void set_login(string login) { _login = login; }
+	inline void set_password(string password) { _password = password; }
+	inline void set_my_id(int id) { my_id = id; }
 
 	void set_recv_mess(string mess, string name_sender);
 	void set_send_mess(string mess, string name_recver);
 
 	void print_all_send_massage();
-	void print_recv_massage();
+	void print_all_recv_massage();
 
+	inline bool is_empty_recv() 
+	{
+		if (recv_mess.size() == 0)
+			return true;
+		else return false;
+	}
+	inline bool is_empty_send() 
+	{ 
+		if (send_mess.size() == 0) 
+			return true; 
+		else return false;
+	}
 
 protected:
 	int my_id;
@@ -78,8 +81,8 @@ public:
 		data.push_back(new_acc);
 	}
 
-	bool containsLog(string login); 
-	int get_id_by_login(string login); 
+	bool containsLog(string login);
+	int get_id_by_login(string login);
 	int get_id_by_name(string name);
 
 
@@ -91,4 +94,3 @@ protected:
 	int count; // counter of accs
 	vector<Account> data;
 };
-
